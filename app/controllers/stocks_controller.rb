a0b174acb927f6a1d5339155dd6e61007d8656d7
dr.Cavkic <dr.cavkic@gmail.com>
2018-04-23 23:49:02 +0200

    ajax i errori

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,8 +1,25 @@
 class StocksController < ApplicationController
 
 	def search 
-		@stock = Stock.new_from_lookup(params[:stock])
-		render 'users/myportfolio'
+		 if params[:stock].present?
+		    @stock = Stock.new_from_lookup(params[:stock])
+		    if @stock
+		      respond_to do |format|
+		        format.js { render partial: 'users/result' }
+		      end
+		    else
+		       respond_to do |format|
+			      flash.now[:danger] = "You have entered an incorrect symbol"
+			      format.js {render partial: 'users/result'}
+			  end
+		    end
+		 else
+		    respond_to do |format|
+			      flash.now[:danger] = "You have entered an empty symbol"
+			      format.js {render partial: 'users/result'}
+			  end
+		 end
+		
 
 	end
 
